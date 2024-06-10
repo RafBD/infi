9900c44552adafc20416d1a21c2cf907992c23f7
RaffaBD <rafa.bediaz@gmail.com>
2024-06-09 20:29:16 -0600

    added logout url

--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -3,5 +3,15 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 export const LogOutButton = () => {
   const { logout } = useAuth0();
-  return <button onClick={() => logout()}>Log Out</button>;
+  return (
+    <button
+      onClick={() =>
+        logout({
+          logoutParams: { returnTo: "https://rafbd.github.io/infi/" },
+        })
+      }
+    >
+      LogOut
+    </button>
+  );
 };
